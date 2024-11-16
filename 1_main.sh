80e350738c9a12014c7710db7ca24941c4335c2d
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2024-11-16 23:49:03 +0900

    For debug

---
+++ 1_main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=designite
+#SBATCH --output=logs/main_%A_%a.out
+#SBATCH --error=errors/main_%A_%a.err
+#SBATCH --array=1-300
+#SBATCH --time=100:00:00
+#SBATCH --partition=cluster_long
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+
+
+echo bash main.bash $SLURM_JOBID
