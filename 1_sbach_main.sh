11619054de4ed4af435f95e49b16c42dbdd6c5cb
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2024-11-17 17:37:25 +0900

    server setting change

--- 1_sbach_main.sh
+++ 1_sbach_main.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=designite
 #SBATCH --output=logs/main_%A_%a.out
 #SBATCH --error=errors/main_%A_%a.err
-#SBATCH --array=1-500
-#SBATCH --time=100:00:00
-#SBATCH --partition=cluster_long
+#SBATCH --array=1-5
+#SBATCH --time=4:00:00
+#SBATCH --partition=cluster_short
 #SBATCH --ntasks=1
-#SBATCH --mem=64G
-#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
 
 mkdir -p errors
 mkdir -p logs
